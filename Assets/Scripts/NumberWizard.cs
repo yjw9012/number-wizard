d2b3bee72bc300e98fef51500f54a1a8ae78b756
yjw9012 <yjw9012@hotmail.com>
2016-09-26 23:16:13 -0700

    First commit - print out "Hello World"

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
